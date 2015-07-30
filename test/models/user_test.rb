c73c9f9d2f0c0b2106f6a008e26f697a8719b867
Daniel Llinas <danllinas@gmail.com>
2015-07-30 15:42:11 -0400

    Added user validation test.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(first_name: "Example", last_name: "User", email: "user@example.com", address: "1234 W Wyncode Lane", city: "Miami", state: "FL")
-    @user.password = "foobar"
-    @user.password_confirmation = "foobar"
-    p @user.valid?
-    p @user.errors.messages
-    @user.save
+    @user = User.new(first_name: "Example", last_name: "User", email: "user@example.com", password: 'foobar', password_confirmation: 'foobar', address: "1234 W Wyncode Lane", city: "Miami", state: "FL")
   end
 
   test "should be valid" do
