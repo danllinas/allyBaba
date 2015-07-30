55c25496dd34d0e11cc7e312ddccae54ed888b1d
Daniel Llinas <danllinas@gmail.com>
2015-07-30 18:38:23 -0400

    Changed scrape item.

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,7 +1,7 @@
 class Deal < ActiveRecord::Base
 
   def self.scrape
-    response = HTTParty.get("http://www.alibaba.com/product-detail/Vewell-84-inch-Stand-TV_60275078333.html")
+    response = HTTParty.get("http://www.alibaba.com/product-detail/2015-latest-designer-ladies-padded-jacket_60220234681.html")
     dom = Nokogiri::HTML(response.body)
   end
 
