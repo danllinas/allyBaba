becb5dc21b9eacf093323bc28dea8e7585d917a8
Daniel Llinas <danllinas@gmail.com>
2015-07-30 17:29:12 -0400

    User update now works. Refactored form into a partial.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:success] = "User successfully created."
+    redirect_to @user
   end
 
   def create
