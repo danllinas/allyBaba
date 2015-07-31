73af742f101983e8ac60142c55c579f1d17d4f2b
Daniel Llinas <danllinas@gmail.com>
2015-07-31 11:43:39 -0400

    Finished User Model tsts. Modified controllers and views.

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,9 +1,13 @@
 class DealsController < ApplicationController
+  before_action :set_deal, only: [:show, :edit, :update, :destroy]
+
   def index
     @response = Deal.scrape
+    @deals = Deal.all
   end
 
   def new
+    @deal = Deal.new
   end
 
   def edit
@@ -11,4 +15,31 @@ class DealsController < ApplicationController
 
   def show
   end
+
+  def create
+    if @current_user
+      @deal = Deal.new(deal_params)
+    else
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+    if @deal.save
+      flash[:success] = "You've created a new deal!"
+      redirect to @current_user
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def deal_params
+    params.require(:deal).permit(:url, :expired, :min_order, :title, :image, :retail_price, :wholesale_price)
+  end
+
+  def set_deal
+    @deal = Deal.find(params[:id])
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,64 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test "name should be present" do
+    @user.first_name = ""
+    @user.last_name = ""
+    refute @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "      "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.first_name = "a" * 51
+    @user.last_name = "b" * 51
+    assert_not @user.valid?
+  end
+
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email should be in valid format" do
+    valid_addresses = %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn)
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test "validation should reject invalid email addresses" do
+    invalid_addresses = %w(user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com)
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid? "#{invalid_address.inspect} should be invalid."
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil diget" do
+    refute @user.authenticated?('')
+  end
+
 end
