db84b6dc90d1db60ce3827a96f4e2989f7883d9a
Daniel Llinas <danllinas@gmail.com>
2015-07-31 12:36:54 -0400

    Passing tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,29 +1,37 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
+  setup do
+    @user = users(:michael)
+  end
+
+  test "should show user" do
+    get :show, id: @user
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @user
     assert_response :success
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
+  test "should update user" do
+    patch :update, id: @user, user: { email: @user.email, first_name: @user.first_name }
+    assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password: @user.password, password_confirmation: @user.password_confirmation, address: @user.address, city: @user.city, state: @user.state }
+    end
+    assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_path
   end
 
 end
