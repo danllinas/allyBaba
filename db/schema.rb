8cb7c949b1b408e89ae6d1f2d526c671f734eb12
Daniel Llinas <danllinas@gmail.com>
2015-07-31 19:21:00 -0400

    Updated Deal database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731212139) do
+ActiveRecord::Schema.define(version: 20150731231831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "deals", force: :cascade do |t|
     t.string   "url"
-    t.boolean  "expired"
-    t.integer  "min_order"
     t.string   "title"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
