2f6a60f451a3358cc09835a64ea99c92562a9151
Ana De Aza <anadea21@hotmail.com>
2015-08-01 18:39:14 -0400

    created confirmation page

---
+++ app/controllers/confirmation_controller.rb
@@ -0,0 +1,4 @@
+class ConfirmationController < ApplicationController
+	def confirmation
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'orders/index'
 
+  get '/confirmation' => 'confirmation#confirmation'
+  
   root      'deals#index'
   get       'signup'  => 'users#new'
   get       'login'   => 'sessions#new'
@@ -15,6 +17,8 @@ Rails.application.routes.draw do
   resources :users
   resources :deals
   resources :charges
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
