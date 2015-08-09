64ab533daa4c5d489b5a5ebc55bf1160429273e3
Daniel Llinas <danllinas@gmail.com>
2015-08-09 11:53:59 -0400

    Starting form layouts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "AllyBaBa"
+    base_title = "The Bulk"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 
