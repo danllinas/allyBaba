18a0c8db47821e0e7dfbfad477c996a08853c0b0
Daniel Llinas <danllinas@gmail.com>
2015-08-11 13:01:29 -0400

    Updated initializer.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,2 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 'thebulk.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:s3_host_name] = '.s3.amazonaws.com'
